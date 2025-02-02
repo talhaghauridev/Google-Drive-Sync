c360daa7bd7457c0f0c3a41cbec8c583cd39ae5d
Talha Ghauri <talhaghauridev@gmail.com>
2025-02-03 00:22:55 +0500

    add constants in storage service

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
+import 'package:file_upload_app/constants/constants.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class StorageService {
   final _storage = FlutterSecureStorage();
 
   Future<bool> getSignInStatus() async {
-    final data = await _storage.read(key: "userData");
-    print("data: $data");
+    final data = await _storage.read(key: AppConstants.userStorageKey);
     if (data != null) {
       return true;
     }
@@ -14,12 +14,12 @@ class StorageService {
   }
 
   Future<void> saveUserData(Map<String, String?> userData) async {
-    await _storage.write(key: "userData", value: json.encode(userData));
+    await _storage.write(
+        key: AppConstants.userStorageKey, value: json.encode(userData));
   }
 
   Future<Map<String, dynamic>?> getUserData() async {
-    final data = await _storage.read(key: "userData");
-    print("getData: $data");
+    final data = await _storage.read(key: AppConstants.userStorageKey);
     if (data != null) {
       return json.decode(data);
     }
